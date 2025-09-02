246d14f221b27f6ad81a9655a1c1e5d521e02e03
Nicolas Grislain <nicolas.grislain@gmail.com>
2025-09-02 20:41:26 +0200

    ok

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # lean-pq
 
 https://www.postgresql.org/docs/current/libpq.html
+
+# Test environment
+
+To lauch the test env you can run:
+`docker compose -f tests/docker-compose.yml up`
--- libpq/shim.c
+++ libpq/shim.c
@@ -11,6 +11,8 @@ struct connection {
     PGconn *conn;
 };
 
+typedef struct connection Connection;
+
 // struct connection *create_connection(const char *host, const char *port, const char *user, const char *password, const char *database) {
 //     PGconn *conn = PQconnectdb(host, port, user, password, database);
 //     return conn;
