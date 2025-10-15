3d7bc3298df2d52366d859297ea8a94afefd283f
Nicolas Grislain <nicolas.grislain@gmail.com>
2025-10-15 23:52:10 +0200

    ok

--- tests/Test.lean
+++ tests/Test.lean
@@ -10,16 +10,16 @@ open ToString
 
 def f (x: Nat) : Nat := x + 1
 
-def testConnect : EIO LeanPq.Error String := do
+def testConnect : EIO LeanPq.Error PGresult := do
   let conninfo := "host=localhost port=5432 user=postgres password=postgres dbname=postgres"
   let conn ← PqConnectDb conninfo
   let db ← PqDb conn
-  let status ← PqStatus conn
-  return toString status
+  let result ← PqExec conn "SELECT 1"
+  return result
 
 def main : IO Unit := do
-  let status ← testConnect.toIO (fun e => IO.Error.otherError 0 (toString e))
-  IO.println status
+  let result ← testConnect.toIO (fun e => IO.Error.otherError 0 (toString e))
+  -- IO.println result
   IO.println s!"Test"
   -- let keywords := #["host", "port", "user", "password", "dbname"]
   -- let values := #["localhost", "5432", "postgres", "postgres", "postgres"]
